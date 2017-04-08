02774f545a7380c026b422ba1f092456f6b6abf8
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-04-08 16:10:23 -0400

    Fix columns and buttons.

--- src/Root.vue
+++ src/Root.vue
@@ -4,20 +4,18 @@
       <div class="row">
         <div class="col-xs-2 col-xs-offset-1">
           <div class="block-pop"></div>
-          <a v-if="team === 1 || team === 2" tabindex="0" id="pop" class="pop" role="button" data-toggle="popover" data-placement="left" data-trigger="focus" data-content="Select your team!"></a>
+          <a v-if="team === 1 || team === 2" tabindex="0" id="pop" class="pop" role="button" data-toggle="popover" data-placement="left" data-trigger="focus" data-content="Select a team."></a>
         </div>
-        <div class="col-xs-6">
+        <div class="col-xs-3">
           <div class="block"></div>
           <h1 v-if="team === 1 || team === 2"> Team {{ team }} </h1>
           <h1 v-if="team === 0" class="confirmed"> Trading </h1>
-          <span v-if="team === 0" @click="renderTrade()"class="glyphicon glyphicon-random" aria-hidden="true"></span>
           <ul v-if="team === 0">
             <li class="checked">{{team1}}<span class="glyphicon glyphicon-ok" aria-hidden="true"></span></li>
             <li class="checked">{{team2}}<span class="glyphicon glyphicon-ok" aria-hidden="true"></span></li>
           </ul>
           <input v-model="team1" v-if="team === 1" placeholder="Golden State Warriors" autofocus/>
           <input v-model="team2" v-if="team === 2" placeholder="Cleveland Cavaliers" autofocus/>
-          <span v-if="buttonBool && (team === 1 || team === 2)" @click="clickArrow()" class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
           </br>
           </br>
 
@@ -56,6 +54,11 @@
             >{{ team.teamName }}
             </li>
           </transition-group>
+        </div>
+        <div class="col-xs-3">
+          <div class="block-arrow"></div>
+          <span v-if="buttonBool && (team === 1 || team === 2)" @click="clickArrow()" class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+          <span v-if="team === 0" @click="renderTrade()"class="glyphicon glyphicon-random" aria-hidden="true"></span>
         </div>
       </div>
     </div>
@@ -149,12 +152,13 @@ input {
   width: 350px;
   font-size: 24px;
   padding: none;
+  color: #444444;
 }
 input:focus {
   outline: none;
 }
 ::-webkit-input-placeholder {
-  color: #333333;
+  color: #444444;
   font-size: 24px;
   /*text-decoration: underline;*/
 }
@@ -178,6 +182,9 @@ li:hover {
   font-size: 20px;
   transition: 0.5s ease-out;
 }
+.glyphicon-arrow-right {
+  font-size: 40px;
+}
 .glyphicon-arrow-right:hover {
   cursor: pointer;
   color: #33B17D;
@@ -185,9 +192,10 @@ li:hover {
 }
 
 .glyphicon-random {
-  display: inline;
-  float: right;
-  margin-right: 180px;
+  /*display: inline;*/
+  /*float: right;*/
+  margin-top: 5px;
+  margin-right: 190px;
   font-size: 35px;
 }
 
@@ -221,8 +229,9 @@ li.checked:hover {
 
 .popover-content {
   /*background-color: #333333;*/
-  /*color: #fff;*/
+  color: #444444;
   /*border: none;*/
+  font-weight: bold;
 }
 .popover-title {
   /*background-color: #333333;*/
@@ -231,14 +240,15 @@ li.checked:hover {
   width: 1px;
 }
 a {
-  /*margin-top: 350px;*/
-  /*display: inline;*/
   margin-left: 180px;
 }
 .block {
   margin-top: 300px;
 }
 .block-pop {
-  margin-top: 360px;
+  margin-top: 362px;
+}
+.block-arrow {
+  margin-top: 350px;
 }
 </style>
